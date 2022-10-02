ee68bf779cd3eb5a11b6a53d09f86ed29d90ccf4
Murdo Maclachlan <murdomaclachlan@duck.com>
2022-10-02 18:42:13 +0100

    end of practical 4
    Signed-off-by: Murdo Maclachlan <murdomaclachlan@duck.com>

--- Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
+++ Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
@@ -93,20 +93,7 @@ import triangle.abstractSyntaxTrees.vnames.DotVname;
 import triangle.abstractSyntaxTrees.vnames.SimpleVname;
 import triangle.abstractSyntaxTrees.vnames.SubscriptVname;
 import triangle.abstractSyntaxTrees.vnames.Vname;
-import triangle.codeGenerator.entities.AddressableEntity;
-import triangle.codeGenerator.entities.EqualityRoutine;
-import triangle.codeGenerator.entities.FetchableEntity;
-import triangle.codeGenerator.entities.Field;
-import triangle.codeGenerator.entities.KnownAddress;
-import triangle.codeGenerator.entities.KnownRoutine;
-import triangle.codeGenerator.entities.KnownValue;
-import triangle.codeGenerator.entities.PrimitiveRoutine;
-import triangle.codeGenerator.entities.RoutineEntity;
-import triangle.codeGenerator.entities.RuntimeEntity;
-import triangle.codeGenerator.entities.TypeRepresentation;
-import triangle.codeGenerator.entities.UnknownAddress;
-import triangle.codeGenerator.entities.UnknownRoutine;
-import triangle.codeGenerator.entities.UnknownValue;
+import triangle.codeGenerator.entities.*;
 
 public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		ActualParameterSequenceVisitor<Frame, Integer>, ArrayAggregateVisitor<Frame, Integer>,
@@ -159,6 +146,10 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 
 	@Override
 	public Void visitRepeatCommand(RepeatCommand ast, Frame frame) {
+		var loopAddr = emitter.getNextInstrAddr();
+		ast.C.visit(this, frame);
+		ast.E.visit(this, frame);
+		emitter.emit(OpCode.JUMPIF, Machine.falseRep, Register.CB, loopAddr);
 		return null;
 	}
 
@@ -699,6 +690,12 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		writeTableDetails(routineDeclaration);
 	}
 
+	// Decides run-time representation of the bar routine.
+	private final void elaborateStdBarRoutine(Declaration routineDeclaration, Primitive primitive) {
+		routineDeclaration.entity = new BarPrimitiveRoutine(Machine.closureSize);
+		writeTableDetails(routineDeclaration);
+	}
+
 	private final void elaborateStdEqRoutine(Declaration routineDeclaration, Primitive primitive) {
 		routineDeclaration.entity = new EqualityRoutine(Machine.closureSize, primitive);
 		writeTableDetails(routineDeclaration);
@@ -733,6 +730,7 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		elaborateStdPrimRoutine(StdEnvironment.puteolDecl, Primitive.PUTEOL);
 		elaborateStdEqRoutine(StdEnvironment.equalDecl, Primitive.EQ);
 		elaborateStdEqRoutine(StdEnvironment.unequalDecl, Primitive.NE);
+		elaborateStdBarRoutine(StdEnvironment.barDecl, Primitive.BAR);
 	}
 
 	boolean tableDetailsReqd;
