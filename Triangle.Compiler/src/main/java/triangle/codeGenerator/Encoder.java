73a5224d159b978e3d587192d3892c447e8e36c6
BettinaBonfanti <beb00055@students.stir.ac.uk>
2022-10-10 13:19:25 +0100

    added comments

--- Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
+++ Triangle.Compiler/src/main/java/triangle/codeGenerator/Encoder.java
@@ -33,13 +33,7 @@ import triangle.abstractSyntaxTrees.aggregates.MultipleArrayAggregate;
 import triangle.abstractSyntaxTrees.aggregates.MultipleRecordAggregate;
 import triangle.abstractSyntaxTrees.aggregates.SingleArrayAggregate;
 import triangle.abstractSyntaxTrees.aggregates.SingleRecordAggregate;
-import triangle.abstractSyntaxTrees.commands.AssignCommand;
-import triangle.abstractSyntaxTrees.commands.CallCommand;
-import triangle.abstractSyntaxTrees.commands.EmptyCommand;
-import triangle.abstractSyntaxTrees.commands.IfCommand;
-import triangle.abstractSyntaxTrees.commands.LetCommand;
-import triangle.abstractSyntaxTrees.commands.SequentialCommand;
-import triangle.abstractSyntaxTrees.commands.WhileCommand;
+import triangle.abstractSyntaxTrees.commands.*;
 import triangle.abstractSyntaxTrees.declarations.BinaryOperatorDeclaration;
 import triangle.abstractSyntaxTrees.declarations.ConstDeclaration;
 import triangle.abstractSyntaxTrees.declarations.Declaration;
@@ -122,17 +116,20 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		TypeDenoterVisitor<Frame, Integer>, VnameVisitor<Frame, RuntimeEntity> {
 
 	// Commands
+	//An assignment has a variable on the left and an expression on the right.
+	// The expression might be a literal, but it might involve something more complex like a mathematical formula.
+	// So first we call visit on the expression node, to generate the instructions for this expression.
 	@Override
 	public Void visitAssignCommand(AssignCommand ast, Frame frame) {
-		var valSize = ast.E.visit(this, frame);
-		encodeStore(ast.V, frame.expand(valSize), valSize);
+		var valSize = ast.E.visit(this, frame); // first we call visit on the expression node, to generate the instructions for this expression.
+		encodeStore(ast.V, frame.expand(valSize), valSize); //works out the variable’s address and generate the instructions to store the value there.
 		return null;
 	}
 
 	@Override
 	public Void visitCallCommand(CallCommand ast, Frame frame) {
-		var argsSize = ast.APS.visit(this, frame);
-		ast.I.visit(this, frame.replace(argsSize));
+		var argsSize = ast.APS.visit(this, frame); //visit the Actual Parameter Sequence or APS (Arguments) and push them onto the stack
+		ast.I.visit(this, frame.replace(argsSize)); // writes CALL instruction with the correct address to reach routine I (procedure/method)
 		return null;
 	}
 
@@ -141,28 +138,38 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		return null;
 	}
 
+	//After the expression is evaluated, there is a jump forwards to skip the “then” part of the command if the “else” part needs to be executed.
+	// There’s another jump to skip over the “else” part when the “then” part is finished.
+	// Jumping back isn’t too hard, we can keep track of the address we need to jump to.
+	// Jumping forward is more difficult though: how do we know how far to jump, when we’ve not generated the instructions for the next bit of code yet?
+	// Backpatching is the technique that solve this problem
+	// When a forward jump is needed, an incomplete instruction is emitted, with a temporary value like 0
+	// Later, when the destination address is known, the code generator can go back and complete the instruction
 	@Override
 	public Void visitIfCommand(IfCommand ast, Frame frame) {
-		ast.E.visit(this, frame);
+		ast.E.visit(this, frame); // ast.E.visit generates the machine code for the expression
+		//generates a JUMPIF instruction, with the address 0 (last argument) and emit stores the current address in the program
 		var jumpifAddr = emitter.emit(OpCode.JUMPIF, Machine.falseRep, Register.CB, 0);
-		ast.C1.visit(this, frame);
-		var jumpAddr = emitter.emit(OpCode.JUMP, 0, Register.CB, 0);
-		emitter.patch(jumpifAddr);
-		ast.C2.visit(this, frame);
-		emitter.patch(jumpAddr);
+		ast.C1.visit(this, frame); // generates the code for the command C1, which is the "then" part of the command
+		var jumpAddr = emitter.emit(OpCode.JUMP, 0, Register.CB, 0); //JUMP instruction is added to skip over the “else” part of the command.
+		emitter.patch(jumpifAddr); //emitter.patch updates the instruction located at jumpIfAddr with the current location in the machine code program
+		ast.C2.visit(this, frame); //generates the code for C2, the "else" block
+		emitter.patch(jumpAddr); //emitter.patch updates the instruction located at jumpIfAddr with the current location in the machine code program
 		return null;
 	}
 
 	@Override
 	public Void visitLetCommand(LetCommand ast, Frame frame) {
-		var extraSize = ast.D.visit(this, frame);
-		ast.C.visit(this, frame.expand(extraSize));
+		var extraSize = ast.D.visit(this, frame); // calls the visitors needed to generate the code for the declarations
+		ast.C.visit(this, frame.expand(extraSize)); // expands the frame and run all local commands
 		if (extraSize > 0) {
-			emitter.emit(OpCode.POP, extraSize);
+			emitter.emit(OpCode.POP, extraSize); //remove extra data from the top of the stack
 		}
 		return null;
 	}
 
+	// A sequential command is made up of two commands;
+	// so visitSequentialCommand simply calls the visitor on the two child nodes representing those two commands.
 	@Override
 	public Void visitSequentialCommand(SequentialCommand ast, Frame frame) {
 		ast.C1.visit(this, frame);
@@ -181,6 +188,11 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		return null;
 	}
 
+	@Override
+	public Void visitRepeatCommand(RepeatCommand repeatCommand, Frame frame) {
+		return null;
+	}
+
 	// Expressions
 	@Override
 	public Integer visitArrayExpression(ArrayExpression ast, Frame frame) {
@@ -191,11 +203,11 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 	@Override
 	public Integer visitBinaryExpression(BinaryExpression ast, Frame frame) {
 		var valSize = ast.type.visit(this);
-		var valSize1 = ast.E1.visit(this, frame);
-		var frame1 = frame.expand(valSize1);
-		var valSize2 = ast.E2.visit(this, frame1);
-		var frame2 = frame.replace(valSize1 + valSize2);
-		ast.O.visit(this, frame2);
+		var valSize1 = ast.E1.visit(this, frame); // visits first expression (left-side expression)
+		var frame1 = frame.expand(valSize1); // frame is being changed in size as the values from expression is temporarily stored in it
+		var valSize2 = ast.E2.visit(this, frame1); // visits second expression (right-side expression)
+		var frame2 = frame.replace(valSize1 + valSize2); // frame is being changed in size as the values from expression is temporarily stored in it
+		ast.O.visit(this, frame2); //visit operator
 		return valSize;
 	}
 
@@ -232,10 +244,11 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		return valSize;
 	}
 
+	// Gets the value of that integer literal from the AST and emits the low level instruction LOADL
 	@Override
 	public Integer visitIntegerExpression(IntegerExpression ast, Frame frame) {
-		var valSize = ast.type.visit(this);
-		emitter.emit(OpCode.LOADL, ast.IL.getValue());
+		var valSize = ast.type.visit(this); // gets the value of that integer literal from the AST
+		emitter.emit(OpCode.LOADL, ast.IL.getValue()); // emits the low level instruction LOADL (pushes the specified literal value onto the stack)
 		return valSize;
 	}
 
@@ -265,10 +278,11 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		return valSize;
 	}
 
+	// gets the value out of a variable
 	@Override
 	public Integer visitVnameExpression(VnameExpression ast, Frame frame) {
 		var valSize = ast.type.visit(this);
-		encodeFetch(ast.V, frame, valSize);
+		encodeFetch(ast.V, frame, valSize); // find the address in memory that the variable is stored in.
 		return valSize;
 	}
 
@@ -281,11 +295,12 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 	@Override
 	public Integer visitConstDeclaration(ConstDeclaration ast, Frame frame) {
 		var extraSize = 0;
-		if (ast.E.isLiteral()) {
-			ast.entity = new KnownValue(ast.E.type.getSize(), ast.E.getValue());
+		// E is the expression being bound to the constant as part of its declaration
+		if (ast.E.isLiteral()) { // If E is a literal
+			ast.entity = new KnownValue(ast.E.type.getSize(), ast.E.getValue()); // creates a KnownValue entity and attach that to the AST
 		} else {
-			var valSize = ast.E.visit(this, frame);
-			ast.entity = new UnknownValue(valSize, frame);
+			var valSize = ast.E.visit(this, frame); // size of the declaration is returned by this visitor: that’s to keep track of where the top of the stack is
+			ast.entity = new UnknownValue(valSize, frame); // makes an UnknownValue object and attach that to the AST. It just needed to provide the frame object so that we can save the address of the current top of the stack.
 			extraSize = valSize;
 		}
 		writeTableDetails(ast);
@@ -316,19 +331,20 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 	@Override
 	public Integer visitProcDeclaration(ProcDeclaration ast, Frame frame) {
 		var argsSize = 0;
-		var jumpAddr = emitter.emit(OpCode.JUMP, 0, Register.CB, 0);
-		ast.entity = new KnownRoutine(Machine.closureSize, frame.getLevel(), emitter.getNextInstrAddr());
+		var jumpAddr = emitter.emit(OpCode.JUMP, 0, Register.CB, 0); //JUMP instruction uses backpatching to later fill in the address to jump to.
+		// KnownRoutine is the entity created to represent this routine in the AST. It stores the address of the first instruction in the routine and has methods encodeCall and encodeFetch to write out the machine code to access this routine whenever it’s referenced later in the program
+		ast.entity = new KnownRoutine(Machine.closureSize, frame.getLevel(), emitter.getNextInstrAddr()); // creates an entity which is attached to the AST, so we know where to find the routine’s instructions later.
 		writeTableDetails(ast);
 		if (frame.getLevel() == Machine.maxRoutineLevel) {
 			reporter.reportRestriction("can't nest routines so deeply");
 		} else {
-			var frame1 = frame.push(0);
-			argsSize = ast.FPS.visit(this, frame1);
+			var frame1 = frame.push(0); //The routine introduces a new scope level so a frame is added to the stack by calling frame.push().
+			argsSize = ast.FPS.visit(this, frame1); //The FPS (FormalParameterSequence) is visited to decorate the AST with declarations for each of the parameters in the routine (unknown variables and unknown addresses)
 			var frame2 = frame.push(Machine.linkDataSize);
-			ast.C.visit(this, frame2);
+			ast.C.visit(this, frame2);  // C is the command making up the body of the procedure (might be a sequential command made up of lots of others). We visit that to generate the relevant code for those commands
 		}
 		emitter.emit(OpCode.RETURN, argsSize);
-		emitter.patch(jumpAddr);
+		emitter.patch(jumpAddr); // Finally, we patch the address for the jump instruction.
 		return 0;
 	}
 
@@ -610,8 +626,10 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 
 	@Override
 	public Void visitOperator(Operator ast, Frame frame) {
-		var routineEntity = (RoutineEntity) ast.decl.entity;
-		routineEntity.encodeCall(emitter, frame);
+		var routineEntity = (RoutineEntity) ast.decl.entity; // ast.decl.entity looks up for the declaration of the operator, which is provided by the standard environment
+		//EncodeCall is the method that actually generates the instruction
+		routineEntity.encodeCall(emitter, frame); // The specific routine that’s called is specified by “primitive”, which was set by the standard environment
+		// Then the two values on the stack (coming from left and right expression) will be replaced with the result on the stack.
 		return null;
 	}
 
@@ -625,6 +643,7 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		return baseObject;
 	}
 
+	//visitSimpleVname looks up the declaration of the variable in the AST.
 	@Override
 	public RuntimeEntity visitSimpleVname(SimpleVname ast, Frame frame) {
 		ast.offset = 0;
@@ -660,8 +679,10 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 	}
 
 	// Programs
+	// In Triangle a program is made up of a command; so visitProgram simply calls for a visit to the command node that’s next down the AST from the program/root node.
+	// Note that there’s a Frame argument to the visit methods; we’ll make use of that to track the locations of variables in memory
 	@Override
-	public Void visitProgram(Program ast, Frame frame) {
+	public Void visitProgram(Program ast, Frame frame) { // Frame argument tracks the locations of variables in memory
 		return ast.C.visit(this, frame);
 	}
 
@@ -705,17 +726,20 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 		writeTableDetails(routineDeclaration);
 	}
 
+	//Each of these calls decorates the relevant declarations in the AST with “entity” objects containing the primitive representations.
+	//Here they are just represented by names, but these just mask the hexadecimal values that will be written into the binary program
+	//These decorated declarations will be referred to later as we walk the tree to generate code.
 	private final void elaborateStdEnvironment() {
 		tableDetailsReqd = false;
-		elaborateStdConst(StdEnvironment.falseDecl, Machine.falseRep);
-		elaborateStdConst(StdEnvironment.trueDecl, Machine.trueRep);
-		elaborateStdPrimRoutine(StdEnvironment.notDecl, Primitive.NOT);
-		elaborateStdPrimRoutine(StdEnvironment.andDecl, Primitive.AND);
-		elaborateStdPrimRoutine(StdEnvironment.orDecl, Primitive.OR);
+		elaborateStdConst(StdEnvironment.falseDecl, Machine.falseRep); //For built-in constants, these are just their values;
+		elaborateStdConst(StdEnvironment.trueDecl, Machine.trueRep); //For built-in constants, these are just their values;
+		elaborateStdPrimRoutine(StdEnvironment.notDecl, Primitive.NOT); //for operators like not, the "entity" objects are the built-in primitive routines that perform those functions
+		elaborateStdPrimRoutine(StdEnvironment.andDecl, Primitive.AND); //for operators like and, the "entity" objects are the built-in primitive routines that perform those functions
+		elaborateStdPrimRoutine(StdEnvironment.orDecl, Primitive.OR); //for operators like or, the "entity" objects are the built-in primitive routines that perform those functions
 		elaborateStdConst(StdEnvironment.maxintDecl, Machine.maxintRep);
-		elaborateStdPrimRoutine(StdEnvironment.addDecl, Primitive.ADD);
-		elaborateStdPrimRoutine(StdEnvironment.subtractDecl, Primitive.SUB);
-		elaborateStdPrimRoutine(StdEnvironment.multiplyDecl, Primitive.MULT);
+		elaborateStdPrimRoutine(StdEnvironment.addDecl, Primitive.ADD); //for operators like add, the "entity" objects are the built-in primitive routines that perform those functions
+		elaborateStdPrimRoutine(StdEnvironment.subtractDecl, Primitive.SUB); //for operators like sub, the "entity" objects are the built-in primitive routines that perform those functions
+		elaborateStdPrimRoutine(StdEnvironment.multiplyDecl, Primitive.MULT); //for operators like multiply, the "entity" objects are the built-in primitive routines that perform those functions
 		elaborateStdPrimRoutine(StdEnvironment.divideDecl, Primitive.DIV);
 		elaborateStdPrimRoutine(StdEnvironment.moduloDecl, Primitive.MOD);
 		elaborateStdPrimRoutine(StdEnvironment.lessDecl, Primitive.LT);
@@ -750,6 +774,8 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 
 	private void encodeStore(Vname V, Frame frame, int valSize) {
 
+		//v.visit works out the address that’s been allocated to the LEFT variable of the assignment
+		//v.visit will call something like visitSimpleVname, which looks up the declaration of the variable in the AST
 		var baseObject = (AddressableEntity) V.visit(this, frame);
 		// If indexed = true, code will have been generated to load an index value.
 		if (valSize > 255) {
@@ -757,10 +783,12 @@ public final class Encoder implements ActualParameterVisitor<Frame, Integer>,
 			valSize = 255; // to allow code generation to continue
 		}
 
-		baseObject.encodeStore(emitter, frame, valSize, V);
+		//The declaration object, found above on v.visit call, works out the variable’s address and generate the instructions to store the value there
+		//current frame is needed to hold the variables for whatever routine is running
+		baseObject.encodeStore(emitter, frame, valSize, V); //baseObject will be something like a KnownValue or KnownAddress entity.
 	}
 
-	// Generates code to fetch the value of a named constant or variable
+	// Generates code to fetch the value of a named constant or variable ( find the address in memory that the variable is stored in)
 	// and push it on to the stack.
 	// currentLevel is the routine level where the vname occurs.
 	// frameSize is the anticipated size of the local stack frame when
