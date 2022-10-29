4293787442620e009988c3893ce728f3edac84cf
DimitrisKoumousidis <dik00030@students.stir.ac.uk>
2022-10-29 15:53:03 +0300

    Name changes and new unit test

--- Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
@@ -21,21 +21,22 @@ public class TestScanner {
 	public void testHi() throws IOException {
 		compileExpectSuccess("/hi.tri");
 
-		String actualOutput = readTAMOutput("hi.tam");
+		String actualOutput = readTAMFileOutput("hi.tam");
 		String expectedOutput = "Hi!";
 		assertEquals(expectedOutput,actualOutput);
+
 	}
 
 	@Test
 	public void testLoopWhile() throws IOException {
 		compileExpectSuccess("/loopwhile.tri");
 
-		String actualOutput = readTAMOutput("loopwhile.tam");
+		String actualOutput = readTAMFileOutput("loopwhile.tam");
 		String expectedOutput = "ababababa";
 		assertEquals(expectedOutput,actualOutput);
 
 		//Test the folded version of the program
-		String actualOutputFolded = readTAMOutput("loopwhilefolded.tam");
+		String actualOutputFolded = readTAMFileOutput("loopwhilefolded.tam");
 		String expectedOutputFolded = "ababababa";
 		assertEquals(expectedOutputFolded,actualOutputFolded);
 	}
@@ -44,12 +45,12 @@ public class TestScanner {
 	public void testWhileCurly() throws IOException {
 		compileExpectSuccess("/while-curly.tri");
 
-		String actualOutput = readTAMOutput("while-curly.tam");
+		String actualOutput = readTAMFileOutput("while-curly.tam");
 		String expectedOutput = "aaaaa";
 		assertEquals(expectedOutput,actualOutput);
 
 		//Test the folded version of the program
-		String actualOutputFolded = readTAMOutput("while-curlyfolded.tam");
+		String actualOutputFolded = readTAMFileOutput("while-curlyfolded.tam");
 		String expectedOutputFolded = "aaaaa";
 		assertEquals(expectedOutputFolded,actualOutputFolded);
 	}
@@ -58,68 +59,60 @@ public class TestScanner {
 		compileExpectSuccess("/decrement.tri");
 
 		//Test with the number to decrement being 5
-		String actualOutput = readTAMOutput("dectestpositivenum.tam");
+		String actualOutput = readTAMFileOutput("dectestpositivenum.tam");
 		String expectedOutput = "43";
 		assertEquals(expectedOutput,actualOutput);
 
 		//Test with the number to decrement being 0
-		String actualOutput2 = readTAMOutput("dectestzero.tam");
+		String actualOutput2 = readTAMFileOutput("dectestzero.tam");
 		String expectedOutput2 = "-1-2";
 		assertEquals(expectedOutput2,actualOutput2);
 
 		//Test with the number to decrement being -5
-		String actualOutput3 = readTAMOutput("dectestnegativenum.tam");
+		String actualOutput3 = readTAMFileOutput("dectestnegativenum.tam");
 		String expectedOutput3 = "-6-7";
 		assertEquals(expectedOutput3,actualOutput3);
 
 		//TEST THE FOLDED VERSION OF THE PROGRAMS
 
 		//Test with the number to decrement being 5
-		String actualOutputFolded = readTAMOutput("dectestpositivenumfolded.tam");
+		String actualOutputFolded = readTAMFileOutput("dectestpositivenumfolded.tam");
 		String expectedOutputFolded = "43";
 		assertEquals(expectedOutputFolded,actualOutputFolded);
 
 		//Test with the number to decrement being 0
-		String actualOutputFolded2 = readTAMOutput("dectestzerofolded.tam");
+		String actualOutputFolded2 = readTAMFileOutput("dectestzerofolded.tam");
 		String expectedOutputFolded2 = "-1-2";
 		assertEquals(expectedOutputFolded2,actualOutputFolded2);
 
 		//Test with the number to decrement being -5
-		String actualOutputFolded3 = readTAMOutput("dectestnegativenumfolded.tam");
+		String actualOutputFolded3 = readTAMFileOutput("dectestnegativenumfolded.tam");
 		String expectedOutputFolded3 = "-6-7";
 		assertEquals(expectedOutputFolded3,actualOutputFolded3);
 
 		//Test with the number being above the word limit
-		String actualOutput4 = readTAMOutput("decrementoverflow.tam");
+		String actualOutput4 = readTAMFileOutput("decrementoverflow.tam");
 		String expectedOutput4 = "Program has failed due to overflow.";
 		assertEquals(expectedOutput4,actualOutput4);
 
 	}
 
 	@Test
-	public void testHiNewComment() {
-		compileExpectFailure("/hi-newcomment.tri");
-	}
-	
-
-	@Test
-	public void testHiNewComment2() {
-		compileExpectFailure("/hi-newcomment2.tri");
-	}
-	
-
-	@Test
-	public void testBarDemo() {
+	public void testBarDemo() throws IOException {
 		compileExpectSuccess("/bardemo.tri");
+
+		String actualOutputFolded = readTAMFileOutput("bardemo.tam");
+		String expectedOutputFolded = "12100200";
+		assertEquals(expectedOutputFolded,actualOutputFolded);
 	}
-	
+
 
 	@Test
 	public void testRepeatUntil() {
 		compileExpectFailure("/repeatuntil.tri");
 	}
 
-	
+
 	private void compileExpectSuccess(String filename) {
 		// build.gradle has a line sourceSets.test.resources.srcDir file("$rootDir/programs")
 		// which adds the programs directory to the list of places Java can easily find files
@@ -158,12 +151,12 @@ public class TestScanner {
 
 
 
-	private String readTAMOutput(String filename) throws IOException {
+	private String readTAMFileOutput(String fileName) throws IOException {
 
 		//This creates a new process which is meant to enter the command prompt and execute the commands to run the program tam files
 		//Since the build.gradle file sets the programs directory as test directory we have to call "cd.." to go back to the main directory first
 		ProcessBuilder commandPrompt = new ProcessBuilder(
-				"cmd.exe", "/c", "cd.. && java -cp build/libs/Triangle-Tools.jar triangle.abstractMachine.Interpreter" +" " + filename);
+				"cmd.exe", "/c", "cd.. && java -cp build/libs/Triangle-Tools.jar triangle.abstractMachine.Interpreter" +" " + fileName);
 
 		commandPrompt.redirectErrorStream(true);
 		Process p = commandPrompt.start();
@@ -191,4 +184,5 @@ public class TestScanner {
 		}
 		return programOutput;
 	}
+
 }
