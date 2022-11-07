d5cf7f16b961f5e54575c9f7611a7a6eb11e70df
BigManGeorgie <98537252+BigManGeorgie@users.noreply.github.com>
2022-11-07 20:14:29 +0000

    Update TestScanner.java

--- Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestScanner.java
@@ -14,83 +14,86 @@ import triangle.syntacticAnalyzer.SourceFile;
 
 public class TestScanner {
 
-	@Test
-	public void testHi() {
-		compileExpectSuccess("/hi.tri");
-	}
+    @Test
+    public void testHi() {
+        compileExpectSuccess("/hi.tri");
+    }
 
-	@Test
-	public void testAdd() {
-		compileExpectSuccess("/add.tri");
-	}
+    @Test
+    public void testAdd() {
+        compileExpectSuccess("/add.tri");
+    }
 
-	@Test
-	public void testHiNewComment() {
-		compileExpectSuccess("/hi-newcomment.tri");
-	}
-	
+    @Test
+    public void testHiNewComment() {
+        compileExpectSuccess("/hi-newcomment.tri");
+    }
 
-	@Test
-	public void testHiNewComment2() {
-		compileExpectSuccess("/hi-newcomment2.tri");
-	}
 
-	@Test
-	public void testBarDemo() {
-		compileExpectSuccess("/bardemo.tri");
-	}
+    @Test
+    public void testHiNewComment2() {
+        compileExpectSuccess("/hi-newcomment2.tri");
+    }
 
-	@Test
-	public void testRepeatUntil() {
-		compileExpectSuccess("/repeatuntil.tri");
-	}
+    @Test
+    public void testBarDemo() {
+        compileExpectSuccess("/bardemo.tri");
+    }
 
-	@Test
-	public void testDecrement() {compileExpectSuccess("/decrement.tri");
-	}
+    @Test
+    public void testRepeatUntil() {
+        compileExpectSuccess("/repeatuntil.tri");
+    }
 
-	@Test
-	public void testWhileCurly() {compileExpectFailure("/while-curly.tri");
-	}
+    @Test
+    public void testDecrement() {
+        compileExpectSuccess("/decrement.tri");
+    }
 
-	@Test
-	public void testLoopWhile() {compileExpectFailure("/loopWhile.tri");
-	}
-	
-	private void compileExpectSuccess(String filename) {
-		// build.gradle has a line sourceSets.test.resources.srcDir file("$rootDir/programs")
-		// which adds the programs directory to the list of places Java can easily find files
-		// getResource() below searches for a file, which is in /programs 
-		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
-		SourceFile source = SourceFile.fromResource(filename);
-		
-		Scanner scanner = new Scanner(source);
-		ErrorReporter reporter = new ErrorReporter(true);
-		Parser parser = new Parser(scanner, reporter);
-		
-		parser.parseProgram();
-		
-		// we should get to here with no exceptions
-		
-		assertEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
-	}
-	
-	private void compileExpectFailure(String filename) {
-		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
-		SourceFile source = SourceFile.fromResource(filename);
-		Scanner scanner = new Scanner(source);
-		ErrorReporter reporter = new ErrorReporter(true);
-			Parser parser = new Parser(scanner, reporter);
+    @Test
+    public void testWhileCurly() {
+        compileExpectFailure("/while-curly.tri");
+    }
 
-		// we expect an exception here as the program has invalid syntax
-		assertThrows(RuntimeException.class, new ThrowingRunnable() {
-			public void run(){
-				parser.parseProgram();
-			}
-		});
-		
-		// currently this program will fail
-		assertNotEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
-	}
+    @Test
+    public void testLoopWhile() {
+        compileExpectFailure("/loopWhile.tri");
+    }
+
+    private void compileExpectSuccess(String filename) {
+        // build.gradle has a line sourceSets.test.resources.srcDir file("$rootDir/programs")
+        // which adds the programs directory to the list of places Java can easily find files
+        // getResource() below searches for a file, which is in /programs
+        //SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
+        SourceFile source = SourceFile.fromResource(filename);
+
+        Scanner scanner = new Scanner(source);
+        ErrorReporter reporter = new ErrorReporter(true);
+        Parser parser = new Parser(scanner, reporter);
+
+        parser.parseProgram();
+
+        // we should get to here with no exceptions
+
+        assertEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
+    }
+
+    private void compileExpectFailure(String filename) {
+        //SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
+        SourceFile source = SourceFile.fromResource(filename);
+        Scanner scanner = new Scanner(source);
+        ErrorReporter reporter = new ErrorReporter(true);
+        Parser parser = new Parser(scanner, reporter);
+
+        // we expect an exception here as the program has invalid syntax
+        assertThrows(RuntimeException.class, new ThrowingRunnable() {
+            public void run() {
+                parser.parseProgram();
+            }
+        });
+
+        // currently this program will fail
+        assertNotEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
+    }
 
 }
