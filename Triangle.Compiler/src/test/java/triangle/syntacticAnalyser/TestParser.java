92202b69a7c5552d292574f1cb17af6d405252f8
Maria <maf00162@students.stir.ac.uk>
2022-11-08 19:26:50 +0000

    Create TestParser.java

---
+++ Triangle.Compiler/src/test/java/triangle/syntacticAnalyser/TestParser.java
@@ -0,0 +1,71 @@
+package triangle.syntacticAnalyser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThrows;
+
+import java.beans.Transient;
+
+import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
+
+import triangle.ErrorReporter;
+import triangle.syntacticAnalyzer.Parser;
+import triangle.syntacticAnalyzer.Scanner;
+import triangle.syntacticAnalyzer.SourceFile;
+
+public class TestParser {
+	
+    @Test
+    public void testWhile() {
+        compileExpectSuccess("/while.tri");
+    }
+
+    @Test
+    public void testNesting() {
+        compileExpectSuccess("/nesting.tri");
+    }
+
+    @Test
+    public void testDirectories() {
+        compileExpectFailure("/directories.tri");
+    }
+	
+	
+	private void compileExpectSuccess(String filename) {
+		// build.gradle has a line sourceSets.test.resources.srcDir file("$rootDir/programs")
+		// which adds the programs directory to the list of places Java can easily find files
+		// getResource() below searches for a file, which is in /programs 
+		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
+		SourceFile source = SourceFile.fromResource(filename);
+		
+		Scanner testParser = new testParser(source);
+		ErrorReporter reporter = new ErrorReporter(true);
+		Parser parser = new Parser(scanner, reporter);
+		
+		parser.parseProgram();
+		
+		// we should get to here with no exceptions
+		
+		assertEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
+	}
+	
+	private void compileExpectFailure(String filename) {
+		//SourceFile source = SourceFile.ofPath(this.getClass().getResource(filename).getFile().toString());
+		SourceFile source = SourceFile.fromResource(filename);
+		Scanner testParser = new testParser(source);
+		ErrorReporter reporter = new ErrorReporter(true);
+		Parser parser = new Parser(scanner, reporter);
+
+		// we expect an exception here as the program has invalid syntax
+		assertThrows(RuntimeException.class, new ThrowingRunnable() {
+			public void run(){
+				parser.parseProgram();
+			}
+		});
+		
+		// currently this program will fail
+		assertNotEquals("Problem compiling " + filename, 0, reporter.getNumErrors());
+	}
+
+}
