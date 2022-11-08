b53fdc3b9356b144aecd0034a9f41d59c6bfa332
boostaboosh <lcs00023@students.stir.ac.uk>
2022-11-08 11:18:42 +0000

    add new LoopWhileCommand to commands

---
+++ Triangle.Compiler/src/main/java/triangle/abstractSyntaxTrees/commands/LoopWhileCommand.java
@@ -0,0 +1,20 @@
+package triangle.abstractSyntaxTrees.commands;
+
+import triangle.abstractSyntaxTrees.expressions.Expression;
+import triangle.abstractSyntaxTrees.visitors.CommandVisitor;
+import triangle.syntacticAnalyzer.SourcePosition;
+
+public class LoopWhileCommand {
+    public LoopWhileCommand(Expression eAST, Command cAST, SourcePosition position) {
+        super(position);
+        E = eAST;
+        C = cAST;
+    }
+
+    public <TArg, TResult> TResult visit(CommandVisitor<TArg, TResult> v, TArg arg) {
+        return v.visitLoopWhileCommand(this, arg);
+    }
+
+    public Expression E;
+    public final Command C;
+}
